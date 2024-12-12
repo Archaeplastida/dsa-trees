3819695d7bec482106ee862bcda450e739a6c212
Saif <saifisami2005@gmail.com>
2024-12-11 23:31:41 -0500

    sumValues() implemented.

--- tree.js
+++ tree.js
@@ -15,7 +15,17 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    if (!this.root) return 0;
+    let sum = this.root.val;
+    const traverseAndSum = (currentNode) => {
+      for (let descendant of currentNode.children) {
+        sum += descendant.val;
+        if (descendant.children.length > 0) traverseAndSum(descendant);
+      }
+    }
+
+    traverseAndSum(this.root);
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
